e12b794772d1f1a040db48617f3a7a7dd1e2123a
aloshary150 <m2ad100@outlook.com>
2026-01-14 00:06:36 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
-module.exports = new Pool({
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }  // مهم مع Render PostgreSQL
 });
+
+module.exports = pool;

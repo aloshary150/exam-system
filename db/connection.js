26a9eee953aec0c7ca71a25b76b59693af77f5d4
aloshary150 <m2ad100@outlook.com>
2026-01-13 22:19:16 +0200

    Add files via upload

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,5 @@
-
+const { Pool } = require("pg");
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
